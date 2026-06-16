4dc19be5e3b4bf7fb6e0026584876a9c14b1231e
Chinmay24012007 <sachdevachinmay24@gmail.com>
2026-06-16 17:54:34 +0530

    Create Assignment1.py

---
+++ week1/Assignment1.py
@@ -0,0 +1,50 @@
+'''#Q1. finding the area of a rectangle
+a=int(input("enter the length of the recatngle"));
+b=int(input("enter the breadth of a rectangle"));
+area=a*b;
+print("area of the rectangle:",area);
+
+#Q2. finding simple interest 
+p=int(input("enter the principle amount"));
+r=int(input("enter the rate of interest:"));
+t=int(input("enter the time:"));
+si=(p*r*t)/100;
+print("simple interest:",si);
+
+#Q3. converting celsius to fahrenheit
+c=int(input("enter the temprature in celsius"));
+f=(9/5*c)+32;
+print("temprature in farhenheit:",f);
+
+#Q4. calculating average of 3 numbers
+a=int(input("enter the first number:"));
+b=int(input("enter the second number:"));
+c=int(input("enter the third number:"));
+avg=(a+b+c)/3
+print("the average of 3 numbers:",avg);
+
+#Q5. finding square and cube of a number
+n=int(input("enter the number you want square of :"));
+p=int(input("enter the number you want cube of:"));
+square=n*n;
+cube=p*p*p;
+print("square of the number:",square);
+print("cube of the number:",cube);
+
+#Q6. swaping 2 numbrs without 2 variables
+a=int(input("enter the first number:"));
+b=int(input("enter the second number:"));
+(a,b)=(b,a);
+print("first number after swapping:",a);
+print("second number after swapping:",b);'''
+
+#Q7. student report program taking student details
+
+
+
+
+
+
+
+
+
